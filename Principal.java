9c378701b2ffb01d3cd9a462da6eb38d150cdd9d
LipzinXD <felipescamargo806@gmail.com>
2023-10-24 14:34:15 +0000

    Carro

--- Principal.java
+++ Principal.java
@@ -2,5 +2,8 @@ public class Principal {
 
     public static void main(String args){
 
+        Carro car = new Carro();
+        Carro carAro = new Carro(20);
+        Carro carQuatroParametros = new Carro( 20, "Gol quadrado", "Preto", 40000);
     }
 }
