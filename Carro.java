90c0fa79017ee78e262e829cbbad44dd233de306
LipzinXD <felipescamargo806@gmail.com>
2023-10-24 13:43:06 +0000

    Carro

---
+++ Carro.java
@@ -0,0 +1,24 @@
+public class Carro{
+    private String cor;
+    private String modelo;
+    private int aro;
+    private double valor; 
+
+    public Carro(){
+
+
+    }
+    public Carro(int aro){
+        this.aro=aro;
+    }
+    public Carro(int aro, String modelo){
+        this.aro=aro;
+        this.modelo=modelo;
+    }
+    public Carro(int aro, String modelo, String cor, double valor){
+        this.aro=aro;
+        this.modelo=modelo;
+        this.valor=valor;
+        this.cor=cor;
+    }
+}
